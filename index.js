f7b516b9ce2d78aed8cc55ab805303abbda6fc8f
Eugenia <eugenia.kaloshina@gmail.com>
2018-09-07 22:25:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+var katzDeliLine=[]
+
+ function takeANumber(katzDeliLine,name)  {
+    katzDeliLine.push(name)
+   return 'Welcome, ' + name + '. You are number '+ katzDeliLine.length + ' in line.'
+}
+
+function nowServing(katzDeliLine)  {
+   
+  if (katzDeliLine.length===0) {
+    return ('There is nobody waiting to be served!')
+  } else {
+   var shift=katzDeliLine.shift(0)
+   return ('Currently serving '+shift+'.')
+  }
+ } 
+
+function currentLine(katzDeliLine)  {
+  if (katzDeliLine.length===0)  {
+    return ('The line is currently empty.')
+  }
+  else  {
+    return ('The line is currently: 1. '+ katzDeliLine[0] + ', 2. '+ katzDeliLine[1])
+  } 
+}
+
+
+
+
+
+takeANumber(katzDeliLine,'Ada')
+takeANumber(katzDeliLine,'Grace')
+takeANumber(katzDeliLine,'Kent')
+currentLine(katzDeliLine)
+takeANumber(katzDeliLine,'Matz')
+currentLine(katzDeliLine)
+nowServing(katzDeliLine,'Steven')
+currentLine(katzDeliLine)
