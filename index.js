768b2921c306d3984470c3d2a884c25bf9f4dc72
Eugenia <eugenia.kaloshina@gmail.com>
2018-09-07 22:51:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ function nowServing(katzDeliLine)  {
  } 
 
 function currentLine(katzDeliLine)  {
-  if (katzDeliLine.length===0)  {
+  for (let i=0; i<katzDeliLine.length; i++)  {
+    if (katzDeliLine.length===0)  {
     return ('The line is currently empty.')
   }
   else  {
     return ('The line is currently: 1. '+ katzDeliLine[0] + ', 2. '+ katzDeliLine[1])
   } 
+  }
 }
 
 
